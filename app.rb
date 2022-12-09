564a5cc155da17e79d8e0017674c612f609be0df
Kirill <leonov835@yandex.ru>
2022-12-09 14:05:02 +0300

    test

--- app.rb
+++ app.rb
@@ -2,6 +2,6 @@ require 'sinatra'
 
 class App < Sinatra::Base
   get '/' do
-    'Hello World!'
+    'Hello World!!'
   end
 end
--- config/puma.rb
+++ config/puma.rb
@@ -4,11 +4,8 @@ environment "production"
 
 daemonize true
 
-activate_control_app "tcp://127.0.0.1:9293"
-bind "unix://#{root}/tmp/puma.sock"
-pidfile "#{root}/tmp/pids/puma.pid"
-rackup "#{root}/config.ru"
-state_path "#{root}/tmp/pids/puma.state"
+workers 3
+preload_app!
 
 threads_count = ENV.fetch('PUMA_THREADS') { 5 }.to_i
 threads threads_count, threads_count
